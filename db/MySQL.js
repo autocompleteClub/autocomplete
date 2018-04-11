2a6fcc1324f945257d78c2b48394954f85b74f3a
WuPenghe <wupenghe@MacBook-Pro-3.local>
2018-04-10 20:10:18 -0400

    add freq cutoff

--- db/MySQL.js
+++ db/MySQL.js
@@ -14,29 +14,21 @@ sequelize.authenticate()
 
 function createOneWordTable() {
   let bookname = 'the_hunger_games'; //mockingjay, the_hunger_games
-  let words_1 = fs.readFileSync(path.join(__dirname, `./../books/${bookname}.json`));
-  bookname = 'mockingjay';
-  let words_2 = fs.readFileSync(path.join(__dirname, `./../books/${bookname}.json`));
-  words_1 = JSON.parse(words_1.toString());
-  words_2 = JSON.parse(words_2.toString());
-  const words = words_1;
-  for (let word in words_2) {
-    if (words[word]) words[word] += words_2[word];
-    else words[word] = words_2[word];
-  }
-  
+  let words = fs.readFileSync(path.join(__dirname, `./../books/${bookname}.json`));
+  words = JSON.parse(words.toString());
+
   const Word = sequelize.define('word', {
     id:   {type: Sequelize.INTEGER, primaryKey: true, autoIncrement:true},
     str:   Sequelize.STRING,
     freq:  Sequelize.INTEGER
   });
-  const options = {force:true, logging:false};
-  const wordsArr = Object.keys(words);
-  const wordsTable = [];
-  wordsArr.forEach((word) => {
-    wordsTable.push({str: word, freq: words[word]});
-  })
 
+  const wordsTable = [];
+  for (let word in words) {
+    wordsTable.push({str: word, freq: words[word]});
+  }
+
+  const options = {force:true, logging:false};
   Word.sync(options);
   Word.bulkCreate(wordsTable)
       .then( () => console.log('TABLE CREATED'))
--- db/textParser.js
+++ db/textParser.js
@@ -1,8 +1,12 @@
-
 const fs = require('fs');
 const path = require('path');
 
 const words = {};  // key is the word, value is frequency
+const phrases = {}; // key is the phrase, value is frequency
+
+const LEAST_PREQ_PHRASE = 5;
+const LEAST_PREQ_WORD = 4;
+
 
 const bookname = 'the_hunger_games'; //mockingjay
 let book = fs.readFileSync(path.join(__dirname, `./../books/${bookname}.txt`));
@@ -12,10 +16,40 @@ book = book.replace(/[\n]/g,' ');
 book = book.replace(/[^a-z' ]/g,'');
 const wordsArr = book.split(' ').filter( word => word.length!==0);
 
-wordsArr.forEach( (word) => {
-  if(words[word]) words[word]++;
-  else words[word] = 1;
-})
+console.log('number of total words:',wordsArr.length);
 
+function phrasecount() {
+  for(let i=0; i<wordsArr.length-1; i++) {
+    const str = wordsArr[i] + ' ' + wordsArr[i+1];
+    if (phrases[str]) phrases[str]++;
+    else phrases[str] = 1;
+  }
 
-fs.writeFileSync(`./books/${bookname}.json`, JSON.stringify(words));
+  for (let str in phrases) {
+    if (phrases[str] < LEAST_PREQ_PHRASE) {
+      delete phrases[str]
+    }
+  }
+
+  console.log('number of unique phrases:', Object.keys(phrases).length);
+  fs.writeFileSync(`./books/phrases.json`, JSON.stringify(phrases));
+}
+
+function wordcount() {
+  wordsArr.forEach( (word) => {
+    if(words[word]) words[word]++;
+    else words[word] = 1;
+  })
+
+  for (let word in words) {
+    if (words[word] < LEAST_PREQ_WORD) {
+      delete words[word]
+    }
+  }
+
+  console.log('number of unique word:', Object.keys(words).length);
+  fs.writeFileSync(`./books/${bookname}.json`, JSON.stringify(words));
+}
+
+wordcount();
+phrasecount();
